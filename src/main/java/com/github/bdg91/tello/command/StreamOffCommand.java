40cb70aea55c52f5a3f2f00c7256acd0f7a64dcc
Bas de Groot <basdegroot.91@gmail.com>
2019-04-19 13:43:38 +0200

    Implement stream off command

---
+++ src/main/java/com/github/bdg91/tello/command/StreamOffCommand.java
@@ -0,0 +1,53 @@
+/*
+ * MIT License
+ *
+ * Copyright © 2019 Bas de Groot
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+package com.github.bdg91.tello.command;
+
+import com.github.bdg91.tello.client.TelloClient;
+
+import java.io.IOException;
+
+/**
+ * This command will disable the video stream.
+ */
+public class StreamOffCommand implements Command {
+
+    private static final String COMMAND = "streamoff";
+
+    private final TelloClient telloClient;
+
+    public StreamOffCommand(final TelloClient telloClient) {
+        this.telloClient = telloClient;
+    }
+
+    /**
+     * Executes the streamoff {@link Command}.
+     *
+     * @return 'ok' if everything is okay, 'error' otherwise
+     * @throws IOException if the sending the command or receiving the return value fails
+     */
+    public String execute() throws IOException {
+        return telloClient.sendCommand(COMMAND);
+    }
+}
