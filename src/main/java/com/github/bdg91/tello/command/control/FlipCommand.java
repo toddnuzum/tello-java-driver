79cc6b13c66303302a22c9b7c0d41a7997a773e5
Bas de Groot <basdegroot.91@gmail.com>
2019-04-20 16:31:15 +0200

    Implement flip command

---
+++ src/main/java/com/github/bdg91/tello/command/control/FlipCommand.java
@@ -0,0 +1,64 @@
+/*
+ * MIT License
+ *
+ * Copyright © 2019 Bas de Groot
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+package com.github.bdg91.tello.command.control;
+
+import com.github.bdg91.tello.client.TelloClient;
+import com.github.bdg91.tello.command.Command;
+import com.github.bdg91.tello.command.model.FlipDirection;
+
+import java.io.IOException;
+
+/**
+ * This command will make the drone flip in the specified direction.
+ */
+public class FlipCommand implements Command {
+
+    private static final String COMMAND = "flip";
+    private static final String SPACE = " ";
+
+    private final TelloClient telloClient;
+    private final FlipDirection direction;
+
+    /**
+     * Creates a flip command.
+     *
+     * @param telloClient  the tello client
+     * @param direction    the direction
+     */
+    public FlipCommand(final TelloClient telloClient, final FlipDirection direction) {
+        this.telloClient = telloClient;
+        this.direction = direction;
+    }
+
+    /**
+     * Executes the flip {@link Command}.
+     *
+     * @return 'ok' if everything is okay, 'error' otherwise
+     * @throws IOException if sending the command or receiving the return value fails
+     */
+    public String execute() throws IOException {
+        return telloClient.sendCommand(COMMAND + SPACE + direction.getLabel());
+    }
+}
